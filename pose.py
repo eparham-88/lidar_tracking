fix to deal with rotation issue

pose.py
@@ -276,8 +276,6 @@ if __name__=="__main__":
                 m, n = match
                 if m.distance < 0.7 * n.distance:
                     matches.append(m)
-                    
-        # prev_frame.find_world_coordinates_whole_img(my_lidar)
 
         prev_frame.find_world_coordinates(matches, True, my_lidar)
 
@@ -294,18 +292,18 @@ if __name__=="__main__":
         # print(M @ np.array([[0], [0], [0], [1]]))
         
         # transform first previous frame
-        this_xyz_transformed = M @ this_frame.xyz
+        this_frame.xyz = M @ this_frame.xyz
 
         if False:
             plt.figure("points")
             ax = plt.axes(projection='3d')
-            ax.scatter3D(this_xyz_transformed[0,:], this_xyz_transformed[1,:], this_xyz_transformed[2,:], color="b")
+            ax.scatter3D(this_frame.xyz[0,:], this_frame.xyz[1,:], this_frame.xyz[2,:], color="b")
             ax.scatter3D(prev_frame.xyz[0,:], prev_frame.xyz[1,:], prev_frame.xyz[2,:], color="r")
             ax.set_xlabel("x")
             ax.set_ylabel('y')
             ax.axis('scaled')
             plt.show()
-        elif True:
+        elif False:
             drawMatches(this_frame, prev_frame, matches)
 
         print(str(i) + " of " + str(len(lst)))